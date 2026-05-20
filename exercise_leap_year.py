503296fd51425bded230de9ae2a50816bd69f87b
MarcosJG123 <mpjuarezgoni@mail.austral.edu.ar>
2026-05-20 14:05:17 -0300

    Update exercise_leap_year.py

--- exercise_leap_year.py
+++ exercise_leap_year.py
@@ -17,4 +17,11 @@ def leap_year():
         Para la entrada "1700", la salida esperada es:
         El año 1700 no es bisiesto
     """
+    year = int(input("Ingrese un año:"))
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        print(f"El año {year} es bisiesto")
+    else:
+        print(f"El año {year} no es bisiesto")
+
     pass
+leap_year()
