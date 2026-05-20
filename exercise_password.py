9ee1f359e928dd5cfe658f619f05ae16891ec289
MarcosJG123 <mpjuarezgoni@mail.austral.edu.ar>
2026-05-20 13:59:39 -0300

    Update exercise_password.py

--- exercise_password.py
+++ exercise_password.py
@@ -23,4 +23,13 @@ def password():
         Contraseña muy corta
         Debe contener un numero
     """
+    contra = input()
+    numero = ("0" in contra or "1" in contra or "2" in contra or "3" in contra or "4" in contra or "5" in contra or "6" in contra or "7" in contra or "8" in contra or "9" in contra)
+    if numero and len(contra) >= 8:
+        print("Contraseña valida")
+    if not numero:
+        print("Debe contener un numero")
+    elif len(contra) < 8:
+        print("Contraseña muy corta")
     pass
+password()
